abfdc37db9efb4bfe96741641a5dddaeb5706eec
Melvin Lau <lau.melvin@icloud.com>
2019-10-15 11:33:40 +0100

    Added up, down, powersavingmode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,31 @@ describe('Thermostat', () => {
   it('starts at 20 degrees', () => {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('.up increases temperature by 1 degree', () => {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('.down decreases temperature by 1 degree', () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a min temperature of 10 C', () => {
+    expect(thermostat.getMinTemperature()).toEqual(10);
+  });
+
+  // it('.down does not decrease temperature below 10 degrees', () => {
+  //   for (let temp = 20; temp < 11; temp--) {
+  //     thermostat.down()
+  //   }
+  //   expect(thermostat.getCurrentTemperature()).toEqual(10);
+  // });
+
+  it('has a power saving mode that is on by default', () => {
+    expect(thermostat.isPowerSavingMode()).toEqual(true);
+  });
+
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,10 +3,26 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.minTemperature = 10;
+    this.powerSavingMode = true;
   }
 
   getCurrentTemperature() {
     return this.temperature;
   }
-}
 
+  up() {
+    this.temperature += 1;
+  }
+
+  down() {
+    this.temperature -= 1;
+  }
+
+  getMinTemperature() {
+    return this.minTemperature;
+  }
+  isPowerSavingMode() {
+    return this.powerSavingMode;
+  }
+}
