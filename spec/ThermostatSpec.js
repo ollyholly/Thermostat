2b2446bd2788ea7f0d91b5055a6cf10b9370d683
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-15 11:17:18 +0100

    Add default temperature

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,11 @@
+describe('Thermostat', () => {
+  let thermostat;
+
+  beforeEach( () => {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', () => {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+  }
+
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+}
+
