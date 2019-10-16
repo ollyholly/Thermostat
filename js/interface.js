b333ac0d12805490a9c2e71e0c7c21dbf2795a48
Heli Sivunen <heli@mooncat.tv>
2019-10-16 16:18:48 +0100

    Add current London weather and button to select city, not connected to API key yet

--- js/interface.js
+++ js/interface.js
@@ -34,5 +34,23 @@ $(document).ready(function () {
     $('#pwm-status').text(thermostat.PSMStatus());
   });
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+   $('#local-temperature').text(data.main.temp);
+});
 
+displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#select-city').val();
+    displayWeather(city);
+});
+
+  function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });}
 });
